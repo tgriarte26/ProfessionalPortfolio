94bfcf74ad6183eeb369ece561a53d4ee1c7eb34
trevor <trevorgriarte@gmail.com>
2026-03-08 20:29:46 -0700

    Adding buttons for games page

--- professional-portfolio/src/app/navPages/GamesPage/page.tsx
+++ professional-portfolio/src/app/navPages/GamesPage/page.tsx
@@ -1,14 +1,16 @@
-import Navbar from '@/components/Navbar/Navbar';
-import styles from '@/app/navPages/GamesPage/GamesPage.module.css';
+import Navbar from "@/components/Navbar/Navbar";
+import styles from "@/app/navPages/GamesPage/GamesPage.module.css";
+import InfoGamesPage from "@/components/InfoGamesPage/InfoGamesPage";
 
 export default function GamesPage() {
   return (
-<div className="flex min-h-screen items-center justify-center bg-white font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-white font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-4xl flex-col justify-between py-10 px-16 bg-white dark:bg-black ">
-        <div className="flex flex-col gap-6 items-start">
+        <div className="flex flex-col gap-6 items-center">
           <Navbar />
+          <InfoGamesPage />
         </div>
       </main>
     </div>
-  )
+  );
 }
---
+++ professional-portfolio/src/components/InfoGamesPage/InfoGamesPage.tsx
@@ -0,0 +1,20 @@
+import styles from "./InfoGamesPage.module.css";
+import Image from "next/link";
+
+export default function InfoGamesPage() {
+  return (
+    <div>
+      <div className={styles.page}>
+        <h1 className={styles.title}>Games</h1>
+        <div className={styles.cardContainer}>
+          <div className={styles.card}>
+            <h2 className={styles.cardTitle}>Pokémon Types</h2>
+          </div>
+          <div className={styles.card}>
+            <h2 className={styles.cardTitle}>Pokémon Higher or Lower</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
